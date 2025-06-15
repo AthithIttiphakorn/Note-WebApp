0f5ff21df333b83e88f21eab66d88ae86a704861
unknown <northitti@gmail.com>
2025-06-15 16:08:50 +0700

    added waitress for production server

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #run when starting web server
 from website import create_app #get create_app func from __init__.py
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True) #turn debug off when running production server
+    serve(app, host='0.0.0.0', port=8000) #turn debug off when running production server
     
